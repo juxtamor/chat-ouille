e836e6974b139a6b5dcc2a7a28d9e5ec0a18d6a5
juxtamor <julien.hzt@gmail.com>
2017-02-17 10:25:06 +0100

    Notre index

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$errors = [];
+$db = mysqli_connect("192.168.1.79","chat-ouille","chat-ouille","chat-ouille"); //URL, Utilisateur, MdP, Base de données//
+session_start();// http://php.net/manual/fr/function.session-start.php
+$access = ["articles", "login", "register", "create_article", "edit_article", "article"];
+$page = "articles";
+if (isset($_GET['page']) && in_array($_GET['page'], $access))
+{
+    $page = $_GET['page'];
+}
+require('apps/traitement_users.php');
+require('apps/traitement_articles.php');
+require('apps/traitement_comments.php');
+require('apps/skel.php');
+?>
