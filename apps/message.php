ed1cbc95ba3f54d6f3382a2144cd86e25bd6afdb
developpementjulio <julio.cruz@outlook.fr>
2017-02-17 16:30:09 +0100

    message.php qui marche

--- apps/message.php
+++ apps/message.php
@@ -1,5 +1,5 @@
 <?php
-$res = mysqli_query($db, "SELECT messages.*, users.login, users.avatar FROM messages, users WHERE users.id = messages.id_author");
+$res = mysqli_query($db, "SELECT messages.*, users.login, users.avatar FROM messages, users WHERE users.id = messages.id_author ORDER BY date DESC" );
 
 while($messages = mysqli_fetch_assoc($res))
 {
