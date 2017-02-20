417aa1301b78bce13145a28ecbdb968195315d9d
developpementjulio <julio.cruz@outlook.fr>
2017-02-20 15:37:32 +0100

    .

--- apps/users-elem.php
+++ apps/users-elem.php
@@ -1,5 +1,5 @@
 <?php
-$res = mysqli_query($db, "SELECT users.* FROM users" );
+$res = mysqli_query($db, "SELECT users.* FROM users WHERE last_act+10>CURRENT_TIMESTAMP");
 while ($users = mysqli_fetch_assoc($res))
 {
 	require('views/users-elem.phtml');
