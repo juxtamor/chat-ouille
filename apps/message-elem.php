56ee722ac5d8baeb86e2583cb7df31933b258910
emeline1 <emeline.zovic@gmail.com>
2017-02-20 11:40:50 +0100

    j

--- apps/message-elem.php
+++ apps/message-elem.php
@@ -1,5 +1,5 @@
 <?php
-$res = mysqli_query($db, "SELECT messages.*, users.login, users.avatar FROM messages, users WHERE users.id = messages.id_author ORDER BY date DESC" );
+$res = mysqli_query($db, "SELECT messages.*, users.login, users.avatar FROM messages, users WHERE users.id = messages.id_author ORDER BY date DESC, login" );
 while ($message = mysqli_fetch_assoc($res))
 {
 	require('views/message-elem.phtml');
